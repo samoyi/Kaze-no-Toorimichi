a0265a177bdf286cd4e940a2be34d01c056f2d41
samoyi <greatlining@163.com>
2018-02-27 20:06:27 +0800

    正在整理各种条目的数据结构

--- Back/module/Item.js
+++ Back/module/Item.js
@@ -8,8 +8,77 @@ Item.prototype = {
     contructor: Item,
 
     // addItem
-    addBook(sISBN, sTitle, sAuthor){
+    addBookItem(sISBN, sTitle, sAuthor, sOfficialSite='', aSubjects){
+        // {
+        //     ISBN: '',
+        //     title: [''],
+        //     author:[''],
+        //     sOfficialSite: '',
+        //     subjects: [
+        //         {
+        //             route: [],
+        //             des: '',
+        //         },
+        //         {
+        //             route: [],
+        //             des: '',
+        //         }
+        //     ]
+        // }
 
+    },
+    addPeriodicalItem(sTitle, sIssue='', sOfficialSite='', aSubjects){
+        // {
+        //     title: [''],
+        //     sIssue:'',
+        //     sOfficialSite: '',
+        //     subjects: [
+        //         {
+        //             route: [],
+        //             des: '',
+        //         },
+        //         {
+        //             route: [],
+        //             des: '',
+        //         }
+        //     ]
+        // }
+
+    },
+    addVideoItem(sTitle, aSubjects, sCataID, sIMDb='', sOfficialSite=''){
+        // {
+        //     IMDb: '',
+        //     title: [''],
+        //     sOfficialSite: '',
+        //     subjects: [
+        //         {
+        //             route: [],
+        //             des: '',
+        //         },
+        //         {
+        //             route: [],
+        //             des: '',
+        //         }
+        //     ]
+        // }
+    },
+
+    addMusic(sTitle, aSubjects, sCataID, sIMDb='', sOfficialSite=''){
+        // {
+        //     IMDb: '',
+        //     title: [''],
+        //     sOfficialSite: '',
+        //     subjects: [
+        //         {
+        //             route: [],
+        //             des: '',
+        //         },
+        //         {
+        //             route: [],
+        //             des: '',
+        //         }
+        //     ]
+        // }
     },
 
     modifyItem(){
